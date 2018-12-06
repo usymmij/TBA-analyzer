0e185c1824581d7bf4e0b3fcae15645349e751f5
usymmij <36722504+usymmij@users.noreply.github.com>
2018-12-06 21:33:04 +0800

    Update apiRetrieve.js

--- apiRetrieve.js
+++ apiRetrieve.js
@@ -1,17 +1,16 @@
-src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'
- 
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
 
- $('button').click(function(){
- var hmm = '';
- var jqxhr = $.ajax({
-    url: 'https://www.thebluealliance.com/api/v3/team/frc4253',
+<script>
+var hmm = '';
+var jqxhr = $.ajax({
+	url: 'https://www.thebluealliance.com/api/v3/team/frc4253',
 	dataType:'HTML',
 	headers: {'X-TBA-Auth-Key': '7zyl3s8GtJlKGQs0jo053ZHYyY4pXaYJuYGNV85sOhF4cUAnyZ6idKGVVm6lLMpw'}    
 })
 $.when(jqxhr).done(function(data) { 
-    $('#demo').replaceWith("");
 	console.log(data);
 	hmm = jQuery.parseHTML(data);
-   $('div').append(hmm);
-   });
- });
+	hmm = JSON.parse(hmm);
+	console.log(hmm);
+});
+</script>
